b3c43a8058468d7ca5f3fd3115845d0af704b3a4
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-07-06 14:03:54 +0100

    Add the bg-texture, pdf, design for header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
 
-const Header = () => <div>Header</div>;
+const Header = () => {
+  return (
+    <header>
+      <div class="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Victor Chukwuemeka</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div>     
+    </header>
+  )
+}
 
-export default Header;
+export default Header
