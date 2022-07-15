2025541fbb4f1622563af25adfe7fa273b8c8c33
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-07-15 23:33:28 +0100

    Add the contact markup

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,20 @@
+/* eslint-disable */
 import React from 'react';
 import './contact.css';
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+        
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
