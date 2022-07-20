f33ae0ac6de4968ad9fefa8458b2bb412c420df4
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-07-20 07:27:48 +0100

    Add the contact section & footer markup

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,22 @@ import './contact.css';
 import { BiEnvelope } from 'react-icons/bi';
 import { RiMessengerLine } from 'react-icons/ri';
 import { BsWhatsapp } from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hn57xv2', 'template_yyqkhzf', form.current, 'fqGaYg0QIYkeoXUQN')
+    e.target.reset()
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -14,25 +28,25 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <BiEnvelope />
+            <BiEnvelope className="contact__option-icon" />
             <h4>Email</h4>
             <h5>anivictor.chukwuemeka.avc@gmail.com</h5>
             <a href="mailto:anivictor.chukwuemeka.avc@gmail.com" target="_blank">Send a message</a>
           </article>
           <article className='contact__option'>
-            <RiMessengerLine />
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>anivictor</h5>
             <a href="https://m.me/ani.victor.chukwuemeka" target="_blank">Send a message</a>
           </article>
           <article className='contact__option'>
-            <BsWhatsapp />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>Whatsapp</h4>
             <h5>+2347065127989</h5>
             <a href="https://api.whatsapp.com/send?phone+2347065127989" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="10" placeholder='Your message' required></textarea>
