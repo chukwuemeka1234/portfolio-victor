96bd7c239c1394b3b2010a71e94e6d59fd200e47
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-09-20 23:52:43 +0100

    Markup changes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ import CV from '../../assets/Resume.pdf';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href={CV} download className='btn'>Get my resume</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
